89bbb7e90397d1412e0590e18bad6fdf7df06df9
Chris Hesterman <spoiler14@comcast.net>
2019-09-29 14:11:33 -0700

    Updates

---
+++ precache-manifest.a734f9e45931b5eb814f1e11cbc65130.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03314b8eacff5eaa941061e577aac339",
+    "url": "/SimpleTodo/index.html"
+  },
+  {
+    "revision": "7f28d754463d6386450e",
+    "url": "/SimpleTodo/static/css/main.4299d037.chunk.css"
+  },
+  {
+    "revision": "e6454bb2bb597cbc918f",
+    "url": "/SimpleTodo/static/js/2.46034e91.chunk.js"
+  },
+  {
+    "revision": "7f28d754463d6386450e",
+    "url": "/SimpleTodo/static/js/main.7c09b89e.chunk.js"
+  },
+  {
+    "revision": "b5f448c96aeadc82a2fb",
+    "url": "/SimpleTodo/static/js/runtime~main.4a46039b.js"
+  }
+]);
